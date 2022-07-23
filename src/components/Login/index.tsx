c470c825017c73ef99f8f84c2b545ae145d6551d
“syedyaseenabbas” <yaseenr@geekyants.com>
2022-07-23 13:47:17 +0530

    feat: Validation on login page

--- src/components/Login/index.tsx
+++ src/components/Login/index.tsx
@@ -1,12 +1,27 @@
 import React, { useState, MouseEvent, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { Button, Col, Container, Form, Navbar, Row, Alert } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Navbar,
+  Row,
+  Alert,
+} from "react-bootstrap";
 import { auth } from "../../Firebase";
+import { Formik } from "formik";
 import {
   signInWithEmailAndPassword,
   GoogleAuthProvider,
   signInWithPopup,
+  UserCredential,
 } from "firebase/auth";
+
+interface Err {
+  email?: string
+  password?: string
+}
 
 const Login: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
@@ -26,49 +41,138 @@ const Login: React.FC = () => {
     }
   };
 
-  const signIn = async (e: MouseEvent<HTMLButtonElement>) => {
-    e.preventDefault();
+  const signIn = async () => {
     setError("");
     try {
-      await signInWithEmailAndPassword(auth, emailRef.current!.value, passwordRef.current!.value);
+      await signInWithEmailAndPassword(
+        auth,
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
       navigate("/");
     } catch (err: any) {
       setError(err.message);
     }
   };
 
-
   return (
     <>
       <Navbar className="justify-content-between" bg="dark" variant="dark">
-        <Navbar.Brand style={{ paddingLeft: "20px" }}>User Authentication</Navbar.Brand>
+        <Navbar.Brand style={{ paddingLeft: "20px" }}>
+          User Authentication
+        </Navbar.Brand>
       </Navbar>
-      <Container style={{ maxWidth: "500px" }} fluid>
-        <Form className="mt-4">
-          <Form.Group controlId="formEmail">
-            {error && <Alert variant="danger">{error}</Alert>}
-            <Form.Label>Email</Form.Label>
-            <Form.Control ref={emailRef} type="email" placeholder="email" />
-          </Form.Group>
-          <Form.Group style={{ marginBottom: "8px" }} controlId="formPassword">
-            <Form.Label style={{ marginTop: "0.8rem" }}>Password</Form.Label>
-            <Form.Control ref={passwordRef} type="password" placeholder="password" />
-          </Form.Group>
-          <Row>
-            <Col xs={4} style={{ width: "100px" }}>
-              <Button onClick={signIn} className="mt-10" type="button" style={{ background: "#2b8be5", marginTop: "10px" }}>
-                Log In
-              </Button>
-            </Col>
-            <Button className="mt-10" onClick={gooleSignIn} type="button" style={{ background: "#2b8be5", width: "130px", marginTop: "10px" }}>
-              Google Login
-            </Button>
-            <Col style={{ marginTop: "11px", width: "275px", marginLeft: "250px" }} xs={8} className="d-flex justify-content-end align-items-center">
-              Don't have an account? <Link style={{ paddingLeft: "5px" }} to="/SignUp">SignUp</Link>
-            </Col>
-          </Row>
-        </Form>
-      </Container>
+      <Formik
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors: Err = {};
+          if (!values.email) {
+            errors.email = "Required";
+          } else if (
+            !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+          ) {
+            errors.email = "Invalid email address";
+          }
+          if (!values.password) {
+            errors.password = "Required";
+          } else if (!/^.{8,32}$/i.test(values.password)) {
+            errors.password = "Minimum length should be 8";
+          }
+          return errors;
+        }}
+        onSubmit={(values) => {
+          signIn()
+        }}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          isSubmitting,
+        }) => (
+          <Container style={{ maxWidth: "500px" }} fluid>
+            <Form className="mt-4">
+              <Form.Group controlId="formEmail">
+                {error && <Alert variant="danger">{error}</Alert>}
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  ref={emailRef}
+                  type="email"
+                  name="email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                  placeholder="email"
+                />
+                <div style={{ color: "red" }}>
+                  {errors.email && touched.email && errors.email}
+                </div>
+              </Form.Group>
+              <Form.Group
+                style={{ marginBottom: "8px" }}
+                controlId="formPassword"
+              >
+                <Form.Label style={{ marginTop: "0.8rem" }}>
+                  Password
+                </Form.Label>
+                <Form.Control
+                  ref={passwordRef}
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.password}
+                  placeholder="password"
+                />
+                <div style={{ color: "red" }}>
+                  {errors.password && touched.password && errors.password}
+                </div>
+              </Form.Group>
+              <Row>
+                <Col xs={4} style={{ width: "100px" }}>
+                  <Button
+                    onClick={() => handleSubmit()}
+                    className="mt-10"
+                    type="button"
+                    style={{ background: "#2b8be5", marginTop: "10px" }}
+                  >
+                    Log In
+                  </Button>
+                </Col>
+                <Button
+                  className="mt-10"
+                  onClick={gooleSignIn}
+                  type="button"
+                  style={{
+                    background: "#2b8be5",
+                    width: "130px",
+                    marginTop: "10px",
+                  }}
+                >
+                  Google Login
+                </Button>
+                <Col
+                  style={{
+                    marginTop: "11px",
+                    width: "275px",
+                    marginLeft: "250px",
+                  }}
+                  xs={8}
+                  className="d-flex justify-content-end align-items-center"
+                >
+                  Don't have an account?{" "}
+                  <Link style={{ paddingLeft: "5px" }} to="/SignUp">
+                    SignUp
+                  </Link>
+                </Col>
+              </Row>
+            </Form>
+          </Container>
+        )}
+      </Formik>
     </>
   );
 };
--- src/components/SignUp/index.tsx
+++ src/components/SignUp/index.tsx
@@ -1,9 +1,17 @@
 import { Link, useNavigate } from "react-router-dom";
 import React, { useRef, MouseEvent, useState } from "react";
-import { Button, Col, Container, Form, Navbar, Row, Alert } from "react-bootstrap";
+import {
+  Button,
+  Col,
+  Container,
+  Form,
+  Navbar,
+  Row,
+  Alert,
+} from "react-bootstrap";
 import { auth } from "../../Firebase";
 import { createUserWithEmailAndPassword } from "firebase/auth";
-import { ErrorMessage, Formik } from 'formik';
+import { Formik } from "formik";
 
 const Signup: React.FC = () => {
   const emailRef = useRef<HTMLInputElement>(null);
@@ -15,7 +23,11 @@ const Signup: React.FC = () => {
     e.preventDefault();
     setError("");
     try {
-      await createUserWithEmailAndPassword(auth, emailRef.current!.value, passwordRef.current!.value);
+      await createUserWithEmailAndPassword(
+        auth,
+        emailRef.current!.value,
+        passwordRef.current!.value
+      );
       navigate("/Login");
     } catch (err: any) {
       setError(err.message);
@@ -25,24 +37,25 @@ const Signup: React.FC = () => {
   return (
     <>
       <Navbar className="justify-content-between" bg="dark" variant="dark">
-        <Navbar.Brand style={{ paddingLeft: "20px" }}>User Authentication</Navbar.Brand>
+        <Navbar.Brand style={{ paddingLeft: "20px" }}>
+          User Authentication
+        </Navbar.Brand>
       </Navbar>
       <Formik
-        initialValues={{ email: '', password: '' }}
-        validate={values => {
-          const errors = { email: '', password: '' };
+        initialValues={{ email: "", password: "" }}
+        validate={(values) => {
+          const errors = { email: "", password: "" };
           if (!values.email) {
-            errors.email = 'Required';
+            errors.email = "Required";
           } else if (
             !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
           ) {
-            errors.email = 'Invalid email address';
+            errors.email = "Invalid email address";
           }
           if (!values.password) {
-            errors.password = 'Required';
-          }
-          else if (!/^.{8,32}$/i.test(values.password)) {
-            errors.password = 'Minimum length should be 8';
+            errors.password = "Required";
+          } else if (!/^.{8,32}$/i.test(values.password)) {
+            errors.password = "Minimum length should be 8";
           }
           return errors;
         }}
@@ -65,24 +78,59 @@ const Signup: React.FC = () => {
               <Form className="mt-4">
                 <Form.Group controlId="formEmail">
                   <Form.Label>Email</Form.Label>
-                  <Form.Control ref={emailRef} type="email" name="email" onChange={handleChange} onBlur={handleBlur}
-                    value={values.email} placeholder="email" />
-                  <div style={{ color: "red" }}>{errors.email && touched.email && errors.email}</div>
+                  <Form.Control
+                    ref={emailRef}
+                    type="email"
+                    name="email"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.email}
+                    placeholder="email"
+                  />
+                  <div style={{ color: "red" }}>
+                    {errors.email && touched.email && errors.email}
+                  </div>
                 </Form.Group>
-                <Form.Group style={{ marginBottom: "8px" }} controlId="formPassword">
-                  <Form.Label style={{ marginTop: "0.8rem" }}>Password</Form.Label>
-                  <Form.Control ref={passwordRef} type="password" name="password" onChange={handleChange} onBlur={handleBlur}
-                    value={values.password} placeholder="password" />
-                  <div style={{ color: "red" }}>{errors.password && touched.password && errors.password}</div>
+                <Form.Group
+                  style={{ marginBottom: "8px" }}
+                  controlId="formPassword"
+                >
+                  <Form.Label style={{ marginTop: "0.8rem" }}>
+                    Password
+                  </Form.Label>
+                  <Form.Control
+                    ref={passwordRef}
+                    type="password"
+                    name="password"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    value={values.password}
+                    placeholder="password"
+                  />
+                  <div style={{ color: "red" }}>
+                    {errors.password && touched.password && errors.password}
+                  </div>
                 </Form.Group>
                 <Row>
                   <Col xs={4}>
-                    <Button onClick={createAccount} className="mt-10" type="button" style={{ background: "#2b8be5", marginTop: "10px" }}>
+                    <Button
+                      onClick={createAccount}
+                      className="mt-10"
+                      type="button"
+                      style={{ background: "#2b8be5", marginTop: "10px" }}
+                    >
                       Sign Up
                     </Button>
                   </Col>
-                  <Col style={{ marginTop: "11px" }} xs={8} className="d-flex justify-content-end align-items-center">
-                    Already have an account? <Link style={{ paddingLeft: "5px" }} to="/Login">Login</Link>
+                  <Col
+                    style={{ marginTop: "11px" }}
+                    xs={8}
+                    className="d-flex justify-content-end align-items-center"
+                  >
+                    Already have an account?{" "}
+                    <Link style={{ paddingLeft: "5px" }} to="/Login">
+                      Login
+                    </Link>
                   </Col>
                 </Row>
               </Form>
